9de16e0f781af12bbd4d7123cdb196291449db8b
James Thompson <james@thomps.onl>
2023-10-04 13:04:52 -0400

    Remove MTGJSON fetch behavior.

--- app/models/mtgjson/card.rb
+++
@@ -1,3 +0,0 @@
-class MTGJSON::Card < MTGJSON::Record
-  self.primary_key = "uuid"
-end
--- spec/models/mtgjson/card_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe MTGJSON::Card do
-  subject(:card) { MTGJSON::Card.order("RANDOM()").first }
-
-  its(:id) { is_expected.to eq(card.uuid) }
-  its(:readonly?) { is_expected.to be(true) }
-end
