2c7cee64b762dd02d51fcce759eb3cef98566061
James Thompson <james@thomps.onl>
2023-10-04 12:54:23 -0400

    Add CardSet model.

---
+++ db/migrate/20231004154736_create_card_sets.rb
@@ -0,0 +1,17 @@
+class CreateCardSets < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension "pgcrypto"
+
+    create_table :card_sets do |t|
+      t.uuid :scryfall_id, index: {unique: true}
+      t.string :code
+      t.string :name
+      t.date :released_on
+      t.string :set_type
+      t.integer :card_count
+      t.string :icon_svg_uri
+
+      t.timestamps
+    end
+  end
+end
